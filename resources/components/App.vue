06c4505760c1317889416e5858c71e50cf1662b7
Esteban-Fernandez18 <esteban.fernandez@iteesa.edu.hn>
2024-09-11 09:18:14 -0500

    registroCtrl

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         Hola Mundo
+        <a href="/login">Login</a>
     </div>
 </template>
 <script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\login\loginCtrl;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',[loginCtrl::class, 'loginView']);
